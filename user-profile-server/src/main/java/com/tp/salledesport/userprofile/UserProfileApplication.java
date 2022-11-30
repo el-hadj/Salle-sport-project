0c01651f2236b121dca1b0b82cc983d858306445
el-hadj <elhadj.balde@etu.u-pec.fr>
2022-11-30 17:24:49 +0100

    adding a bean

--- user-profile-server/src/main/java/com/tp/salledesport/userprofile/UserProfileApplication.java
+++ user-profile-server/src/main/java/com/tp/salledesport/userprofile/UserProfileApplication.java
@@ -1,13 +1,24 @@
 package com.tp.salledesport.userprofile;
 
 
+import com.tp.salledesport.userprofile.repository.CoachProfileRepository;
+import com.tp.salledesport.userprofile.repository.UserProfileRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class UserProfileApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(UserProfileApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner runner(CoachProfileRepository coachProfileRepository, UserProfileRepository userProfileRepository){
+        return (args -> {
+
+        });
     }
 }
